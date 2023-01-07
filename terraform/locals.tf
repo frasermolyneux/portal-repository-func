66127b7d40373c78f549cf4353be70e9eb3efbf5
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-07 20:00:48 +0000

    Rename storage account

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,5 +3,5 @@ locals {
   key_vault_name            = "kv-${random_id.environment_id.hex}-${var.location}"
   app_insights_name         = "ai-ptl-repo-func-${random_id.environment_id.hex}-${var.environment}-${var.location}"
   function_app_name         = "fa-ptl-repo-func-${random_id.environment_id.hex}-${var.environment}-${var.location}"
-  function_app_storage_name = "saptlrepofunc${random_id.environment_id.hex}"
+  function_app_storage_name = "saptlrepofn${random_id.environment_id.hex}"
 }
