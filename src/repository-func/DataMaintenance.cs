9c9b38e91010285ae8d9772a10c8c98362e9f179
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-08 10:17:59 +0000

    Migrating functions implementation

---
+++ src/repository-func/DataMaintenance.cs
@@ -0,0 +1,31 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+using XtremeIdiots.Portal.RepositoryApiClient;
+
+namespace XtremeIdiots.Portal.RepositoryFunc;
+
+public class DataMaintenance
+{
+    private readonly ILogger<DataMaintenance> _log;
+    private readonly IRepositoryApiClient _repositoryApiClient;
+
+    public DataMaintenance(
+        ILogger<DataMaintenance> log,
+
+        IRepositoryApiClient repositoryApiClient)
+    {
+        _log = log;
+        _repositoryApiClient = repositoryApiClient;
+    }
+
+    [FunctionName("DataMaintenance")]
+    public async Task RunDataMaintenance([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
+    {
+        _log.LogInformation("Performing Data Maintenance");
+
+        await _repositoryApiClient.DataMaintenance.PruneChatMessages();
+        await _repositoryApiClient.DataMaintenance.PruneGameServerEvents();
+        await _repositoryApiClient.DataMaintenance.PruneGameServerStats();
+    }
+}
