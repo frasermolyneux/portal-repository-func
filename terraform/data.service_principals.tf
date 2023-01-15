37edce34807c0282e67a6a1e44e31c3af0ea5bde
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-15 11:09:50 +0000

    Update name

--- terraform/data.service_principals.tf
+++ terraform/data.service_principals.tf
@@ -3,7 +3,7 @@ data "azuread_service_principal" "repository_api" {
 }
 
 data "azuread_service_principal" "servers_integration_api" {
-  display_name = format("portal-servers-integration-api-%s", var.environment)
+  display_name = format("portal-servers-integration-%s", var.environment)
 }
 
 data "azuread_service_principal" "geolocation_api" {
