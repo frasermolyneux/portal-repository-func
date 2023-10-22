cea240bfc4edc29d638b5bff9dd5440e438749a3
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-10-22 20:50:27 +0100

    Disable APIM subscription tracing

--- terraform/api_subscriptions.tf
+++ terraform/api_subscriptions.tf
@@ -5,6 +5,7 @@ resource "azurerm_api_management_subscription" "geolocation_api_subscription" {
   resource_group_name = data.azurerm_api_management.platform.resource_group_name
 
   state = "active"
+  allow_tracing = false
 
   api_id       = data.azurerm_api_management_api.geolocation_api.id
   display_name = format("%s-%s", local.function_app_name, data.azurerm_api_management_api.geolocation_api.name)
@@ -17,6 +18,7 @@ resource "azurerm_api_management_subscription" "repository_api_subscription" {
   resource_group_name = data.azurerm_api_management.platform.resource_group_name
 
   state = "active"
+  allow_tracing = false
 
   api_id       = data.azurerm_api_management_api.repository_api.id
   display_name = format("%s-%s", local.function_app_name, data.azurerm_api_management_api.repository_api.name)
@@ -29,6 +31,7 @@ resource "azurerm_api_management_subscription" "servers_integration_api_subscrip
   resource_group_name = data.azurerm_api_management.platform.resource_group_name
 
   state = "active"
+  allow_tracing = false
 
   api_id       = data.azurerm_api_management_api.servers_integration_api.id
   display_name = format("%s-%s", local.function_app_name, data.azurerm_api_management_api.servers_integration_api.name)
