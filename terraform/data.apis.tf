52beb17cf9201a70ccc04e8def066a43344f7656
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-06 08:32:55 +0100

    Switching over to new instance

--- terraform/data.apis.tf
+++ terraform/data.apis.tf
@@ -1,19 +1,15 @@
 data "azurerm_api_management_api" "repository_api" {
-  provider = azurerm.api_management
-
   name                = var.repository_api.apim_api_name
-  api_management_name = data.azurerm_api_management.platform.name
-  resource_group_name = data.azurerm_api_management.platform.resource_group_name
+  api_management_name = data.azurerm_api_management.core.name
+  resource_group_name = data.azurerm_api_management.core.resource_group_name
 
   revision = var.repository_api.apim_api_revision
 }
 
 data "azurerm_api_management_api" "servers_integration_api" {
-  provider = azurerm.api_management
-
   name                = var.servers_integration_api.apim_api_name
-  api_management_name = data.azurerm_api_management.platform.name
-  resource_group_name = data.azurerm_api_management.platform.resource_group_name
+  api_management_name = data.azurerm_api_management.core.name
+  resource_group_name = data.azurerm_api_management.core.resource_group_name
 
   revision = var.servers_integration_api.apim_api_revision
 }
