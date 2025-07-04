b93e7840c0eeb28e0f81e76db96f771453efd570
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-07-04 22:52:39 +0100

    Update app settings to include repository and servers base URLs in function app configuration

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -38,7 +38,9 @@ resource "azurerm_linux_function_app" "app" {
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE"                   = "1"
     "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
-    "apim_base_url"                              = data.azurerm_api_management.core.gateway_url
+
+    "repository_base_url" = data.azurerm_api_management.core.gateway_url
+    "servers_base_url"    = data.azurerm_api_management.core.gateway_url
 
     "portal_repository_apim_subscription_key_primary"   = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret_primary.name)
     "portal_repository_apim_subscription_key_secondary" = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret_secondary.name)
