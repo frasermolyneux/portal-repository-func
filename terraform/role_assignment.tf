220bcd25276e5b65bcc545db9ec98e1400a94192
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-12 19:40:23 +0000

    Update function app identity references to use unified repository function identity

--- terraform/role_assignment.tf
+++ terraform/role_assignment.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "app-to-storage" {
   scope                = azurerm_storage_account.function_app_storage.id
   role_definition_name = "Storage Blob Data Owner"
-  principal_id         = local.repository_funcapp_identity.principal_id
+  principal_id         = local.repository_func_identity.principal_id
 }
