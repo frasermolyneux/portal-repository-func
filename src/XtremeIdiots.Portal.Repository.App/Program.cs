1432e3fca3a245a29199ff4cff18dc5231ff9b1e
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-06 19:57:49 +0100

    Remove obsolete tests and functions related to game server stats, ban file monitoring, live log updates, and live stats. Clean up StringExtensions and Program.cs by removing unused dependencies and configurations.

--- src/XtremeIdiots.Portal.Repository.App/Program.cs
+++ src/XtremeIdiots.Portal.Repository.App/Program.cs
@@ -10,8 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using MX.Api.Client.Extensions;
-using MX.GeoLocation.Api.Client.V1;
-using XtremeIdiots.Portal.Integrations.Servers.Api.Client.V1;
 using XtremeIdiots.Portal.Repository.Api.Client.V1;
 using XtremeIdiots.Portal.Repository.App;
 
@@ -38,9 +36,6 @@ var host = new HostBuilder()
             {
                 options.Connect(new Uri(appConfigEndpoint), credential)
                     .Select("RepositoryApi:*", environmentLabel)
-                    .Select("ServersIntegrationApi:*", environmentLabel)
-                    .Select("GeoLocationApi:*", environmentLabel)
-                    .Select("XtremeIdiots:*", environmentLabel)
                     .Select("XtremeIdiots.Portal.Repository.App:*", environmentLabel)
                     .TrimKeyPrefix("XtremeIdiots.Portal.Repository.App:")
                     .ConfigureRefresh(refresh =>
@@ -74,21 +69,6 @@ var host = new HostBuilder()
         services.AddRepositoryApiClient(options => options
             .WithBaseUrl(configuration["RepositoryApi:BaseUrl"] ?? throw new InvalidOperationException("RepositoryApi:BaseUrl configuration is required"))
             .WithEntraIdAuthentication(configuration["RepositoryApi:ApplicationAudience"] ?? throw new InvalidOperationException("RepositoryApi:ApplicationAudience configuration is required")));
-
-        services.AddServersApiClient(options =>
-        {
-            options.WithBaseUrl(configuration["ServersIntegrationApi:BaseUrl"] ?? throw new ArgumentNullException("ServersIntegrationApi:BaseUrl"))
-                .WithEntraIdAuthentication(configuration["ServersIntegrationApi:ApplicationAudience"] ?? throw new ArgumentNullException("ServersIntegrationApi:ApplicationAudience"));
-        });
-
-        services.AddGeoLocationApiClient(options =>
-        {
-            options.WithBaseUrl(configuration["GeoLocationApi:BaseUrl"] ?? throw new ArgumentNullException("GeoLocationApi:BaseUrl"))
-                .WithApiKeyAuthentication(configuration["GeoLocationApi:ApiKey"] ?? throw new ArgumentNullException("GeoLocationApi:ApiKey"))
-                .WithEntraIdAuthentication(configuration["GeoLocationApi:ApplicationAudience"] ?? throw new ArgumentNullException("GeoLocationApi:ApplicationAudience"));
-        });
-
-        services.AddMemoryCache();
 
         services.AddHealthChecks();
     })
